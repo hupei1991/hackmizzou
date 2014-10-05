f1e6516a883d9513b2f86c4bb99d5e2198016f4e
HangCui <oscarcuihang@hotmail.com>
2014-10-04 21:56:33 -0500

    delete dam files and update

--- tessel-temp-master/sendtempandhumid.js
+++ tessel-temp-master/sendtempandhumid.js
@@ -11,22 +11,20 @@ var request = require('request');
 var climate = climatelib.use(tessel.port['A']);
 var ambient = ambientlib.use(tessel.port['B']);
 
+setImmediate(function loop () {
 	climate.on('ready', function () {
 	  	console.log('Connected to si7020');
-			setImmediate(function loop () {
-
+			
 			climate.readTemperature('f', function (err, temp) {
 					//console.log("Read temp: " + temp.toFixed(4));
 					
 				climate.readHumidity(function (err, humid) {
 				    console.log('Degrees:', temp.toFixed(4) + 'F', 'Humidity:', humid.toFixed(4) + '%RH');
-	setTimeout(loop, 300);
-				   
 				    //request.post('http://requestb.in/15gylj71', {json: {humidity: humid.toFixed(4), degrees: temp.toFixed(4) }}); 	 
 				});
 			});
 	});
-/*
+
 	ambient.on('ready', function () {
 	 // Get points of light and sound data.
 	  	setInterval( function () {
@@ -40,8 +38,7 @@ var ambient = ambientlib.use(tessel.port['B']);
 		}); // The readings will happen every .5 seconds unless the trigger is hit
  	}); 
 	//request.post('http://requestb.in/15gylj71', {json: {humidity: humid.toFixed(4), degrees: temp.toFixed(4), lightlevel: ldata.toFixed(8), soundlevel: sdata.toFixed(8) }}); 	 
-	
-	setTimeout(loop, 300);*/
+	setTimeout(loop, 300);
 });
 
 climate.on('error', function(err) {
